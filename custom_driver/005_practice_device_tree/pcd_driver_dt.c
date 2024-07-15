874743e265fc593f6187e5b848a77cc5b73d4e87
Nguyen Hung Phong <phong.nguyenbeo2k2@hcmut.edu.vn>
2024-07-15 11:23:40 +0700

    Status: done part 1 and have tested on the board. Implement part 2 but doesn't test on the board

--- custom_driver/005_practice_device_tree/pcd_driver_dt.c
+++ custom_driver/005_practice_device_tree/pcd_driver_dt.c
@@ -8,6 +8,9 @@
 #include <linux/uaccess.h>
 #include <linux/slab.h>
 #include <linux/mod_devicetable.h>
+#include <linux/stdarg.h>
+#include <linux/of.h>
+#include <linux/of_device.h>
 
 /*define macro*/
 #define AUTHOR          "phong_foolish - gmail:phongqh01@gmail.com"
@@ -26,6 +29,7 @@ ssize_t phong_read (struct file *filp, char __user *user_buff, size_t count, lof
 int phong_platform_driver_probe (struct platform_device *pdev);
 int phong_platform_driver_remove(struct platform_device *pdev);
 int phong_release (struct inode *p_inode, struct file *filp);
+device_data_t* get_device_info_from_dt(struct device *dev);
 
 enum pcdev_name
 {
@@ -44,8 +48,9 @@ struct device_data pcdrv_device_data[] =
     [DEVICEA1x] = {.property_1 = 12, .property_2 = 9},
     [DEVICEB1x] = {.property_1 = 9, .property_2 = 2002}
 };
-
+/*That variable is used for managing variable within driver*/
 driver_data_t drv_data;
+
 const struct of_device_id dt_list_match[] = 
 {
     {.compatible = "pcdev-A1x", .data = (void *)DEVICEA1x},
@@ -61,6 +66,9 @@ struct file_operations pcd_fops =
     .release = phong_release,
 	.owner = THIS_MODULE
 };
+/*the kernel is support device tree, that mean CONFIG_OF in the .config file is y. */
+/*used of_match_ptr function macro for detecting that the kernel is support device tree. */
+
 struct platform_driver phong_platform_driver =
 {
     .probe = phong_platform_driver_probe,
@@ -68,7 +76,7 @@ struct platform_driver phong_platform_driver =
     .driver = 
     {
         .name = "phong_char_device",
-        .of_match_table = dt_list_match
+        .of_match_table = of_match_ptr(dt_list_match)
     }
 };
 
@@ -121,6 +129,46 @@ static void __exit phong_driver_exit(void)
     unregister_chrdev_region(drv_data.device_number_base, MAX_DEVICE);
 }
 
+device_data_t* get_device_info_from_dt(struct device *dev)
+{
+    device_data_t *dev_data;
+    int ret;
+    struct device_node *dev_node = dev->of_node;
+    if (dev_node == NULL)
+    {
+        pr_info("This probe didn't happen because of device tree node.\n");
+        return NULL;
+    }
+    dev_data = devm_kzalloc(dev, sizeof(*dev_data), GFP_KERNEL);
+    if (dev_data == NULL)
+    {
+        pr_info("Can't allocate memory.\n");
+        return ERR_PTR(-ENOMEM);
+    }
+    /*read property org,device-serial-num*/
+    ret = of_property_read_string(dev_node,"org,device-serial-num",&dev_data->serial_number);
+    if (ret != 0)
+    {
+        pr_info("Missing serial number property.\n");
+        return ERR_PTR(-EINVAL);
+    }
+    /*read property org,size*/
+    ret = of_property_read_u32(dev_node, "org,size", &dev_data->size);
+    if (ret != 0)
+    {
+        pr_info("Missing size property.\n");
+        return ERR_PTR(-EINVAL);
+    }
+    /*read permission property*/
+    ret = of_property_read_u32(dev_node, "org,perm",&dev_data->perm);
+    if (ret !=0)
+    {
+        pr_info("Missing permission propterty.\n");
+        return ERR_PTR(-EINVAL);
+    }
+
+    return dev_data;
+}
 int phong_open (struct inode *p_inode, struct file *filp)
 {
     pr_info("Opening deivce is successful");
@@ -148,11 +196,90 @@ int phong_release (struct inode *p_inode, struct file *filp)
 }
 int phong_platform_driver_probe (struct platform_device *pdev)
 {
+    /*List task*/
+    /*1. Check the whether the device is created by device tree.*/
+    /*2. Get the info of device from device tree and initialize device.*/
+    /*3. Print the info of device which is pick up from device node.*/
+    /*4. Dynamically allocate for buff field on struct pcdev_private_data*/
+    /*5. Get the device number.*/
+    /*6. Do cdev init with file operations and register cdev with kernel.*/
+    /*7. Create device file.*/
+
+    /*Now let's start*/
+    device_data_t *dev_data;
+    struct device *dev = &pdev->dev;
+    device_drv_data_t *dev_drv_data;
+    int driver_data;
+    const struct of_device_id *match;
+    int ret;
+    /*1. Check the whether the device is created by device tree*/
+    /*match will always be NULL if LINUX doesnt support device tree i.e CONFIG_OF is off*/
+    match = of_match_device(of_match_ptr(dt_list_match),dev);
+    if(match == NULL)
+    {
+        pr_info("The device is matched by device tree. Because kernel don't support device tree!\n");
+        return -ENODEV;
+    }
+    /*2. Get the info of device from device tree and initialize device.*/
+    dev_data = get_device_info_from_dt(dev);
+    if(IS_ERR(dev_data))
+    {
+        return PTR_ERR(dev_data);
+    }
+    driver_data = (long)match->data;
+    /*Save the pcd_received_data for else function can use it*/
+    dev_drv_data->pdata.size = dev_data->size;
+    dev_drv_data->pdata.serial_number = dev_data->serial_number;
+    dev_drv_data->pdata.perm = dev_data->perm;
+    /*3. Print the info of device which is pick up from device node.*/
+    pr_info ("size of detected device is %d\n",dev_drv_data->pdata.size);
+    pr_info ("serial number of detected device is %s\n ",dev_drv_data->pdata.serial_number);
+    pr_info ("permission of detected device is %d\n",dev_drv_data->pdata.perm);
+    pr_info("Config item 1 is %d\n", pcdrv_device_data[driver_data].property_1);
+    pr_info("Config item 2 is %d\n", pcdrv_device_data[driver_data].property_2);    
+    dev_set_drvdata(dev, dev_drv_data);
+    /*4. Dynamically allocate for buff field on struct device_drv_data*/
+    dev_drv_data->buffer = devm_kzalloc(dev,dev_drv_data->pdata.size, GFP_KERNEL);
+    if (!dev_drv_data->buffer)
+    {
+        pr_info("Can't allocate memory.\n");
+        return -ENOMEM;
+    }
+    /*5. Get the device number.*/
+    dev_drv_data->device_number = drv_data.device_number_base + drv_data.total_devices;
+    /*6. Do cdev init with file operations and register cdev with kernel.*/
+    cdev_init(&dev_drv_data->pcd_cdev, &pcd_fops);
+    dev_drv_data->pcd_cdev.owner = THIS_MODULE;
+    ret = cdev_add(&dev_drv_data->pcd_cdev, dev_drv_data->device_number, 1);
+    if (ret < 0)
+    {
+        pr_err("cdev add failed.\n");
+        return ret;
+    }
+    /*7. Create device file into the class.*/
+    drv_data.pcd_device = device_create(drv_data.pcd_class, dev, dev_drv_data->device_number, NULL, "pcd_device_file-%d", drv_data.total_devices);
+    if (IS_ERR(drv_data.pcd_device))
+    {
+        pr_err("Device file creation is failed!.\n");
+        ret = PTR_ERR(drv_data.pcd_device);
+        cdev_del(&dev_drv_data->pcd_cdev);
+        return ret;
+    }
+
+    drv_data.total_devices++;
     pr_info("The device is matched with driver!\n");
     return 0;
 }
 int phong_platform_driver_remove(struct platform_device *pdev)
 {
+    device_drv_data_t *dev_drv_data;
+    dev_drv_data = (device_drv_data_t *)dev_get_drvdata(&pdev->dev);
+    /*1. Remove a device file that created by device_create API in the class directory.*/
+    device_destroy(drv_data.pcd_class, dev_drv_data->device_number);
+    /*2. remove cdev that registered with VFS of kernel*/
+    cdev_del(&dev_drv_data->pcd_cdev);
+
+    drv_data.total_devices--;
     pr_info("The device is removed from the system!\n");
     return 0;
 }
