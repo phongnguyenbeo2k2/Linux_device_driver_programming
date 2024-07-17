70acba747db2978880baa24ad0bec97ef33f0e41
Nguyen Hung Phong <phong.nguyenbeo2k2@hcmut.edu.vn>
2024-07-17 09:24:54 +0700

    Before update the code when practice 5 is completed.

--- custom_driver/007_gpio_sysfs/007_practice_gpiosysfs.c
+++ custom_driver/007_gpio_sysfs/007_practice_gpiosysfs.c
@@ -1,26 +1,31 @@
-#include <linux/module.h>
-#include <linux/fs.h>
-#include <linux/device.h>
-#include <linux/cdev.h>
-#include <linux/platform_device.h>
-#include <linux/kdev_t.h>
-#include <linux/uaccess.h>
-#include <linux/slab.h>
-#include <linux/mod_devicetable.h>
-#include <linux/stdarg.h>
-#include <linux/of.h>
-#include <linux/of_device.h>
+#include "gpiosysfs_driver_data.h"
 
 /*define macro*/
 #define AUTHOR          "phong_foolish - gmail:phongqh01@gmail.com"
 
 /*format the sting is printed to console by prefix the name of function is called and the line*/
 #undef pr_fmt
-#define pr_fmt(fmt) "%s (line %d): "fmt,__func__,__LINE__ 
+#define pr_fmt(fmt)     "%s (line %d): "fmt,__func__,__LINE__ 
 
 /*declare function prototype and struct variable*/
+/*----------------function----------------------*/
 static int __init gpio_sysfs_init(void);
 static void __exit gpio_sysfs_exit(void);
+int gpio_sysfs_probe (struct platform_device *pdev);
+int gpio_sysfs_remove (struct platform_device *pdev);
+/*----------------variable----------------------*/
+
+struct platform_driver gpio_sysfs_platform_driver = 
+{
+    .probe = gpio_sysfs_probe,
+    .remove = gpio_sysfs_remove,,
+    .driver =
+    {
+        .name = "gpio_sysfs_driver"
+    }
+};
+
+driver_data_t gpio_drv_data;
 
 /*begin detail function*/
 static int __init gpio_sysfs_init(void)
---
+++ custom_driver/007_gpio_sysfs/gpiosysfs_driver_data.h
@@ -0,0 +1,29 @@
+/*Define struct variable for platform driver*/
+#ifndef GPIOSYSFS_DRIVER_DATA
+#define GPIOSYSFS_DRIVER_DATA
+
+#include <linux/module.h>
+#include <linux/fs.h>
+#include <linux/device.h>
+#include <linux/cdev.h>
+#include <linux/platform_device.h>
+#include <linux/kdev_t.h>
+#include <linux/uaccess.h>
+#include <linux/slab.h>
+#include <linux/mod_devicetable.h>
+#include <linux/stdarg.h>
+#include <linux/of.h>
+#include <linux/of_device.h>
+
+typedef struct 
+{
+    char label[20];
+}gpio_drv_data_t;
+
+typedef struct
+{
+    int total_device;
+    struct class *gpio_sysfs_class;
+}driver_data_t;
+
+#endif
