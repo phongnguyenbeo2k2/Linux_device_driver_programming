25adc2e79543185dae8ad8c9d53a48dd089d6947
Nguyen Hung Phong <phong.nguyenbeo2k2@hcmut.edu.vn>
2024-07-22 15:58:26 +0700

    Done stage 3 (part 2) and wait to test on the board

--- custom_driver/007_gpio_sysfs/gpiosysfs_driver_data.h
+++ custom_driver/007_gpio_sysfs/gpiosysfs_driver_data.h
@@ -13,16 +13,21 @@
 #include <linux/mod_devicetable.h>
 #include <linux/of.h>
 #include <linux/of_device.h>
+#include <linux/gpio/consumer.h>
 
+#define OFF     0
+#define ON      1
 typedef struct 
 {
     char label[20];
+    struct gpio_desc *desc;
 }gpiodev_drv_data_t;
 
 typedef struct
 {
     int total_device;
     struct class *gpio_sysfs_class;
+    struct device **dev_sys;
 }driver_data_t;
 
 #endif
